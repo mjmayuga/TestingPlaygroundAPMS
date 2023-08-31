712b780343628de11400f14715c35af5c4ecf3a5
mjmayuga <58869361+mjmayuga@users.noreply.github.com>
2023-09-01 00:13:41 +0800

    Add files via upload

--- arduinoGet.cpp
+++ arduinoGet.cpp
@@ -1,55 +1,23 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <libserial/SerialStream.h> // Install the SerialStream library for serial communication
+#include <SerialStream.h>  // This library might be different depending on your OS
+
+using namespace std;
+using namespace LibSerial;
 
 int main() {
-    using namespace LibSerial;
-    
-    // Serial configuration for Arduino connected via USB
-    SerialStream arduinoUSB1;
-    arduinoUSB1.Open("/dev/ttyUSB0");
-    arduinoUSB1.SetBaudRate(SerialStreamBuf::BAUD_9600);
-    
-    SerialStream arduinoUSB2;
-    arduinoUSB2.Open("/dev/ttyUSB1");
-    arduinoUSB2.SetBaudRate(SerialStreamBuf::BAUD_9600);
-    
-    // Wifi communication with Arduino connected to Orange Pi
-    // Replace these values with the actual IP and port
-    std::string arduinoIP = "arduino_ip_address";
-    int arduinoPort = 12345;
-    
-    // Setup networking to communicate with Arduino over WiFi
-    // Implement networking code using sockets or a library like Boost.Asio
-    
-    while (true) {
-        // Read data from USB Arduino 1
-        std::string arduino1Data;
-        getline(arduinoUSB1, arduino1Data);
-        // Parse and process data from arduino1Data
-        
-        // Read data from USB Arduino 2
-        std::string arduino2Data;
-        getline(arduinoUSB2, arduino2Data);
-        // Parse and process data from arduino2Data
-        
-        // Read data from WiFi-connected Arduino
-        // Implement code to receive data from the Arduino over WiFi
-        
-        // Process and combine sensor data
-        
-        // Implement your processing logic here
-        
-        // Print or send data to another system (database, web server, etc.)
-        // For now, let's just print the combined data
-        std::cout << "Combined Sensor Data: " << combinedData << std::endl;
-    }
-    
-    // Close the serial connections
-    arduinoUSB1.Close();
-    arduinoUSB2.Close();
-    
-    return 0;
+  SerialStream arduino("/dev/ttyUSB0", ios_base::out | ios_base::in);  // Open serial port
+  arduino.SetBaudRate(SerialStreamBuf::BAUD_9600);  // Set baud rate
+
+  if (!arduino.good()) {
+    cerr << "Error opening serial port" << endl;
+    return 1;
+  }
+
+  string line;
+  while (true) {
+    getline(arduino, line);  // Read line from Arduino
+    cout << "Received: " << line << endl;
+  }
+
+  return 0;
 }
